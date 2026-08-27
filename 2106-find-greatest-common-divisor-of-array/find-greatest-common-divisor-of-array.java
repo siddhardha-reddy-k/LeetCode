6f5fb362a6af18dce822be4107a5a2441c037140
Siddhardha Reddy K <72437994+siddhardha-reddy-k@users.noreply.github.com>
2026-08-27 10:28:46 +0530

    Time: 6 ms (16.50%) | Memory: 45.8 MB (6.70%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -3,9 +3,9 @@ class Solution {
     public static int findGCD(int n1, int n2) {
         while (n1 > 0 && n2 > 0) {
             if (n1 > n2) {
-                n1 = n1 - n2;
+                n1 = n1 % n2;
             } else {
-                n2 = n2 - n1;
+                n2 = n2 % n1;
             }
         }
 
