92a438e1db6e00823fc60305b9fca9e3aa720c83
Siddhardha Reddy K <72437994+siddhardha-reddy-k@users.noreply.github.com>
2026-08-28 14:24:30 +0530

    Time: 24 ms (80.93%) | Memory: 86.3 MB (46.87%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+    static void sort(int[] nums, int left, int right) {
+        if(left >= right) return;
+
+        int mid = (left + right) / 2;
+        sort(nums, left, mid);
+        sort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+    static void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while(i <= mid && j <= right) {
+            if(nums[i] <= nums[j])
+                temp[k++] = nums[i++];
+            else 
+                temp[k++] = nums[j++];
+        }
+        while(i <= mid)
+            temp[k++] = nums[i++];
+        while(j <= right)
+            temp[k++] = nums[j++];
+
+        for(int x = 0;  x<temp.length; x++) {
+            nums[left + x] = temp[x];
+        }
+
+    }
+}
